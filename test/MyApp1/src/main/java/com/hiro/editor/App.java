05083eb5a613fd0dddcb822bef8cedd287cc4046
v02qabs <takesue090@gmail.com>
2025-02-01 18:56:14 +0900

    add MyApp1

---
+++ test/MyApp1/src/main/java/com/hiro/editor/App.java
@@ -0,0 +1,13 @@
+package com.hiro.editor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test/project-making.sh
@@ -0,0 +1,5 @@
+mvn archetype:generate \
+  -DarchetypeArtifactId=maven-archetype-quickstart \
+  -DinteractiveMode=false \
+  -DgroupId=$1 \
+  -DartifactId=$2
